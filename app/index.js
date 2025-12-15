012b6717ddbdfd58f468b42e2e32a8f17cfb46e5
Aziz403 <azizbenmallouk4@gmail.com>
2025-12-15 19:21:52 +0100

    first commit

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Bissmilllah!');
+  res.send('Bissmilllah, hala!');
 });
 
 // Health check endpoint for end-to-end test
